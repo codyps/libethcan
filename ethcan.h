e5f678af6bc547f0bce6e6d79eb82ea7114090b2
Cody Schafer <cpschafer@gmail.com>
2012-02-20 09:45:58 -0500

    switch to xcb style

--- ethcan.h
+++ ethcan.h
@@ -6,7 +6,7 @@
  *  Interface for interacting with the Ethercan/2.
  */
 
-typedef struct ecan_instance;
+typedef struct ecan_connection_t ecan_connection_t;
 
 /*! defgroup ECAN instance setup
  * @{
@@ -15,21 +15,22 @@ typedef struct ecan_instance;
 /* wrapper for ecan_create which also creates the socket.
  * @hostname: can also be an ip address (as a string).
  * @port:     if zero, default is used. */
-ecan_instance *ecan_open(char *hostname, uint16_t port);
-
-ecan_instance *ecan_create(int udp_sock_fd);
-void           ecan_destroy(ecan_instance *ei);
+ecan_connection_t *ecan_open(char *hostname, uint16_t port);
+ecan_connection_t *ecan_create(int udp_sock_fd);
+void               ecan_destroy(ecan_connection_t *ei);
 
 /*! @defgroup CAN ID managment.
  * @{*/
 
 /* range is inclusive */
-int  ecan_canid_add_range(ecan_instance *ei, uint32_t can_id_start, uint32_t can_id_end);
-//int  ecan_canid_remove(ecan_instance *ei, uint32_t can_id);
-//bool ecan_canid_is_added(ecan_instance *ei, uint32_t can_id);
+int  ecan_canid_add_range(ecan_connection_t *c,
+		uint32_t can_id_start, uint32_t can_id_end);
+//int  ecan_canid_remove(ecan_connection_t *c, uint32_t can_id);
+//bool ecan_canid_is_added(ecan_connection_t *c, uint32_t can_id);
 
 /* wrapper for ecan_canid_add_range */
-static inline int  ecan_canid_add(ecan_instance *ei, uint32_t can_id) {
+static inline int  ecan_canid_add(ecan_connection_t *c, uint32_t can_id)
+{
 	return ecan_canid_add_range(ei, can_id, can_id);
 }
 
@@ -37,8 +38,10 @@ static inline int  ecan_canid_add(ecan_instance *ei, uint32_t can_id) {
 
 /*! @defgroup Sending and Recieving CAN datagrams.
  * @{*/
-int ecan_send_to_can(ecan_instance *ei, uint32_t can_id, void *in_buf, size_t bytes);
-ssize_t ecan_recv_from_can(ecan_instance *ei, void *out_buf, size_t bytes);
+int ecan_send_to_can(ecan_connection_t *c, uint32_t can_id, void *in_buf, size_t bytes);
+ssize_t ecan_recv_from_can(ecan_connection_t *c, void *out_buf, size_t bytes);
 /*!@}*/
+
+void ecan_flush(ecan_con
 
 #endif
